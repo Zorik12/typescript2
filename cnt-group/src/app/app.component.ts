269b7518bdb688aa666b46689c7202d6b7199c6b
Corsa <palanovsep@gmail.com>
2025-11-27 10:29:00 +0300

    qew3

--- cnt-group/src/app/app.component.ts
+++ cnt-group/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'cnt-group';
+  title: string = 'Hello, Angular';
 }
